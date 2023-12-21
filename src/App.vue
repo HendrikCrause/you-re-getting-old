af4cf76f4f5dd2df64a09dbce51fe15e7978337c
hendrik <hcrause@impact.com>
2023-12-21 19:09:24 +0200

    Add tailwindcss, basic home page styles

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-    <router-view></router-view>
+    <div class="grid place-items-center h-dvh bg-gradient-to-br from-blue-900 to-slate-900">
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,9 @@
 <template>
-    <h1>You're Getting Old</h1>
-    <h3>Online Christmas edition</h3>
-
-    <button>
-        <RouterLink to="/game/new">Start a Game</RouterLink>
-    </button>
-    <button>
-        <RouterLink to="/rules">How to Play</RouterLink>
-    </button>
+    <div class="flex flex-col gap-4 items-center">
+        <h1 class="font-serif text-center text-7xl text-white">You're Getting Old</h1>
+        <h3 class="text-center text-2xl text-red-700">Online Christmas edition</h3>
+    
+        <button class="bg-red-800 rounded-lg w-1/2 p-2 text-white mt-8"><RouterLink to="/game/new">Start a Game</RouterLink></button>
+        <button class="bg-green-800 rounded-lg w-1/2 p-2 text-white"><RouterLink to="/rules">How to Play</RouterLink></button>
+    </div>
 </template>
