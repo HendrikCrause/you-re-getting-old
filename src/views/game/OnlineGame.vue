5f7729004e4b6553a5273479930b66f4255f5e8d
hendrik <hcrause@impact.com>
2023-12-25 08:51:35 +0200

    Resize again

--- src/views/game/OnlineGame.vue
+++ src/views/game/OnlineGame.vue
@@ -6,7 +6,7 @@ import client_qr from '@/assets/client-qr.png'
     <BaseLayout>
         <h1 class="font-serif text-center text-5xl text-white">Online Score</h1>
         <p class="text-white text-3xl">Everyone scan this QR code:</p>
-        <img :src="client_qr" class="w-1/3" />
+        <img :src="client_qr" class="w-1/2" />
         <RouterLink to="/client/start" class="text-purple-600 underline text-2xl">Or follow this link</RouterLink>
         <RouterLink to="/game/old" class="bg-green-800 rounded-lg w-1/2 p-5 text-white text-3xl text-center mt-4">Start</RouterLink>
     </BaseLayout>
